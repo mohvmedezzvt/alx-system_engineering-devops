67231f69c9732753531cd43beb5ed8ccd5d52ea3
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-11-24 13:30:52 +0200

    Using Puppet, creating a file in /tmp.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,7 @@
-exec { 'create_school_file':
-    command => '/bin/echo "I love Puppet" > /tmp/school && /bin/chmod 0744 /tmp/school && /bin/chown www-data:www-data /tmp/school',
-    path    => '/usr/bin:/bin',
-    creates => '/tmp/school',
+file { '/tmp/school':
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
