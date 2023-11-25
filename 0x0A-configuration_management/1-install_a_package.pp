3e812032776bb2622d966a840d7208c4f6f374fb
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-11-25 14:57:12 +0200

    Using Puppet, install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Puppet manifest to install flask from pip3.
 
+exec { 'install_werkzeug':
+  command     => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path        => '/usr/local/bin',
+  refreshonly => true,
+}
+
 exec { 'install_flask':
   command     => '/usr/bin/pip3 install Flask==2.1.0',
   path        => '/usr/local/bin',
