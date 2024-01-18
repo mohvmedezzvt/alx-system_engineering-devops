75d073ec49020e0be4b77dd1193ec44491c17caa
root <root@DESKTOP-T659TV6>
2024-01-18 17:37:03 +0200

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers for a given subreddit """
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyPythonScript/1.0'}
+
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
