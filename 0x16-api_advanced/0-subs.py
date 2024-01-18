e74a58a51700aba60f4c855f6af184854a673d0b
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-01-18 18:57:11 +0200

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ returns the number of subscribers for a given subreddit """
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyPythonScript/1.0'}
+    headers = {'User-Agent': 'script/1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
